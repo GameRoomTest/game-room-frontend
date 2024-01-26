143dce72bed5f882dc2d2cdcfb093f6beb8045cb
Joel Flores <joelf.-@live.com>
2024-01-26 00:11:08 -0300

    WIP: Create main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-function App() {
- 
+import { FunctionComponent } from "react";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import MainPage from "./core/main-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+  }
+]);
+
+const App: FunctionComponent = () => {
   return (
-    <>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
-export default App
+export default App;
---
+++ src/core/game/game-list/index.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import GameList from "./game-list.component";
+
+export default styled(GameList)`
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  gap: 1rem;
+  padding-top: 2rem;
+  width: calc(100% - 3rem);
+  max-width: 800px;
+  margin: auto;
+
+  .game-item {
+    border: solid 1px gray;
+    border-radius: 8px;
+
+    .title {
+      font-weight: bold;
+      color: gray;
+    }
+  }
+`;
