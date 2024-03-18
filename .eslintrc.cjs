92ea05cdfa8f19abe1d2e68632d2c65e46b26d5e
Joel Flores <joelf.-@live.com>
2024-03-17 22:11:39 -0300

    Set prettier rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -14,5 +15,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    'max-len': ['error', { code: 100 }],
+    'prettier/prettier': 'warn',
   },
 }
